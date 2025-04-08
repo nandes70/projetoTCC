b9c48c555d51d0c8e022eb06aa29e37634d723cc
leokenta <leonardo.kenta02@gmail.com>
2025-04-08 17:10:40 -0300

    2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {NavComponent} from './component/template/nav/nav.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import {MatListModule } from '@angular/material/list';
+import {FooterComponent } from './component/template/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
