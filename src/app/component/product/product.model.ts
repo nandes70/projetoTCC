0f6637e7d120af152786f5f96b59a0b40649f253
nandes70 <natafernandes190607@gmail.com>
2025-06-14 11:31:45 -0300

    FINALMENTE

--- src/app/component/product/product.model.ts
+++ src/app/component/product/product.model.ts
@@ -1,17 +1,16 @@
-export interface Product
-{
-    proId?: number
-    proNome: string
-    proPrecoCusto: number | null
-    proPrecoVenda: number | null
-    proDescricao: string
-    proEstoque: number | null
-    proCategoria: string
-    proCodigoBarra: string
-    proMarca: string
-    proStatus: boolean | null
-    proFabricante: string
-    proAplicacao: string
-    proDataCadastro: string
-    proDataAtualizacao: string
-}
+export interface Product {
+    proId?: number;                 // Opcional para diferenciar criação (sem id) e atualização (com id)
+    proNome: string;
+    proPrecoCusto: number | null;
+    proPrecoVenda: number | null;
+    proDescricao: string;
+    proEstoque: number | null;
+    proCategoria: string;
+    proCodigoBarra: string;
+    proMarca: string;
+    proStatus: boolean | null;
+    proFabricante: string;
+    proAplicacao: string;
+    proDataCadastro: string;
+    proDataAtualizacao: string;
+  }
