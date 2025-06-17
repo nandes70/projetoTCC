ba72d9eedb5efcaeda484cf8a57db960d6c6c8f6
EllenRoda <programacr001@gmail.com>
2025-06-17 16:47:43 -0300

    atualizacao18

--- src/app/component/template/nav/nav.component.ts
+++ src/app/component/template/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  menuAberto: boolean = true;
 
+  // Detecta movimento do mouse
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    // Se o mouse estiver muito próximo da borda esquerda, abre o menu
+    if (event.clientX < 20) {
+      this.menuAberto = true;
+    }
+
+    // Se o mouse estiver longe do menu, fecha
+    if (event.clientX > 250 && this.menuAberto) {
+      this.menuAberto = false;
+    }
+  }
 }
